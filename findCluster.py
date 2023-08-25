2164bf54b78356339a8ce0ff9bf2d8e7ab02a367
AnhTuan260202 <anhtuan3804@gmail.com>
2023-08-25 15:23:19 +0700

    chuan hoa + kmeans

--- findCluster.py
+++ findCluster.py
@@ -3,11 +3,11 @@ from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 
 # Đọc dữ liệu từ file CSV
-data = pd.read_csv("patient_dataset_normalization.csv")
+data = pd.read_csv("patient_dataset_standardized.csv")
 
 # Chọn các cột để sử dụng trong thuật toán KMeans
-selected_features = ['age', 'chest_pain_type', 'blood_pressure', 'cholesterol', 'max_heart_rate', 'exercise_angina', 'plasma_glucose', 'insulin', 'bmi', 'diabetes_pedigree', 'hypertension', 'heart_disease', 'smoking_status']
-X = data[selected_features]
+# selected_features = ['age', 'chest_pain_type', 'blood_pressure', 'cholesterol', 'max_heart_rate', 'exercise_angina', 'plasma_glucose', 'insulin', 'bmi', 'diabetes_pedigree', 'hypertension', 'heart_disease', 'smoking_status']
+X = data.values
 
 # Tính WCSS (Within-Cluster Sum of Squares) cho các số lượng cụm từ 1 đến max_clusters
 max_clusters = 10
