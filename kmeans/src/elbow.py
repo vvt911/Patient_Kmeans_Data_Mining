11ec0d8c2939b39bdc6766114aecd72353b43d5f
vvt911 <thuyvucuber@gmail.com>
2023-08-26 21:40:00 +0700

    update

--- kmeans/src/elbow.py
+++ kmeans/src/elbow.py
@@ -3,10 +3,8 @@ from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 
 # Đọc dữ liệu từ file CSV
-data = pd.read_csv("patient_dataset_normalization.csv")
+data = pd.read_csv("patient_normalization.csv")
 
-# Chọn các cột để sử dụng trong thuật toán KMeans
-# selected_features = ['age', 'chest_pain_type', 'blood_pressure', 'cholesterol', 'max_heart_rate', 'exercise_angina', 'plasma_glucose', 'insulin', 'bmi', 'diabetes_pedigree', 'hypertension', 'heart_disease', 'smoking_status']
 X = data.values
 
 # Tính WCSS (Within-Cluster Sum of Squares) cho các số lượng cụm từ 1 đến max_clusters
